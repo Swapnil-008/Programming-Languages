8f01e4ae3a8077a5227d616d37ecd6f7d89f4f8f
Swapnil <swapnilshingne155@gmail.com>
2026-06-17 09:33:08 +0530

    Functions file updated

--- Python/08_Functions.py
+++ Python/08_Functions.py
@@ -12,7 +12,7 @@ def sum(a, b): # function defination with parameters
 # Types of functions: Built-in functions and User defined functions
 
 # Built-in functions: Functions provided by python
-# By-default end for print function is \n, but if want to modify it we can modify it by specifying the parameter
+# By-default 'end' for print function is \n, but if want to modify it, we can modify it by specifying the parameter
 # print("Swap", end=" ")
 # print("Yo Yo")  # now this comes on same line
 
@@ -30,6 +30,14 @@ def printList(list):
 
 # printList(list1)
 
+def factorial(num):
+    fact = 1
+    for i in range(1, num + 1):
+        fact *= i
+    return fact
+
+print(factorial(5))
+
 # Recursion:
 # When a function calls itself repeatedly
 
@@ -42,6 +50,7 @@ def printRecursive(n):
 
 printRecursive(10)
 print()
+
 # printing numbers while backtracking
 def printBacktrack(n):
     if(n == 0):
@@ -51,3 +60,19 @@ def printBacktrack(n):
 
 printBacktrack(10)
 print()
+
+# calculating factorial using recursion
+def calc_fact(num):
+    if(num == 0 or num == 1):
+        return 1
+    return num * calc_fact(num - 1)
+
+print(calc_fact(5))
+
+# calculating fibonacci using recursion
+def calc_fibo(num):
+    if num <= 2:
+        return 1
+    return calc_fibo(num - 1) + calc_fibo(num - 2)
+
+print(calc_fibo(5))
